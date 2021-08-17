cbc5c891a3a6903f4f31b3696e6ef8513b68b0a4
Amri Karisma <amrikarisma@yahoo.com>
2021-08-18 02:21:24 +0700

    fix cairer page

--- component-templates/comp-text-list-hover.php
+++ component-templates/comp-text-list-hover.php
@@ -3,22 +3,26 @@
         <div class="category" data-aos="fade-up" data-aos-delay="200">
             <span><?php echo get_sub_field('subtitle'); ?></span>
         </div>
-        <div class="title">
-            <h4 data-aos="fade-left" data-aos-delay="400"><?php echo get_sub_field('title'); ?></h4>
-        </div>
+        <!-- <div class="title">
+            <h4 data-aos="fade-left" data-aos-delay="400"><?php //echo get_sub_field('title'); 
+                                                            ?></h4>
+        </div> -->
         <?php if (have_rows('content')) : ?>
             <div class="content">
                 <div class="wrap-item-list">
                     <?php $itemNumber = 1; ?>
                     <?php while (have_rows('content')) : the_row(); ?>
-                        <div class="item-wrapper-list">
+                        <div class="item-wrapper-list" id="item-wrapper-list_<?php echo $itemNumber; ?>">
+                            <div class="title">
+                                <h4 data-aos="fade-left" data-aos-delay="400"><?php the_sub_field('title'); ?></h4>
+                            </div>
                             <div class="item-list" data-aos="fade-up" data-aos-delay="<?php echo 300 * $itemNumber; ?>">
                                 <span class="item-number"><?php echo '0' . $itemNumber; ?></span>
                                 <span class="item-content"><?php the_sub_field('title'); ?></span>
                             </div>
                             <?php $image = get_sub_field('image'); ?>
                             <?php if ($image) : ?>
-                                <img data-aos="fade-up" data-aos-delay="<?php echo 800 * $itemNumber; ?>" data-aos-duration="3000" class="img-fluid image-list-content" width="400" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+                                <img data-aos="fade-in" data-aos-delay="<?php echo 800 * $itemNumber; ?>" data-aos-duration="3000" class="img-fluid image-list-content" width="400" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
                             <?php endif; ?>
                         </div>
                         <?php $itemNumber++; ?>
